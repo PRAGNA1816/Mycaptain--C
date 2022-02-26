9c7762d15b27a34a54f055af70402214ff61ee2a
PRAGNA1816 <96616690+PRAGNA1816@users.noreply.github.com>
2022-02-26 19:27:50 +0530

    Update switch statement.c

--- switch statement.c
+++ switch statement.c
@@ -5,17 +5,17 @@ int main()
     scanf("%d",&food_item);
     switch(food_item)
     {
-        case 1:printf("food item-Pizza\nprice-Rs239");
+        case 1:printf("food item-Pizza :)\nprice-Rs239");
         break;
-        case 2:printf("food item-Burger\nprice-Rs129");
+        case 2:printf("food item-Burger :)\nprice-Rs129");
         break;
-        case 3:printf("food item-Pasta\nprice-Rs179");
+        case 3:printf("food item-Pasta :)\nprice-Rs179");
         break;
-        case 4:printf("food item-French Fries\nprice-Rs99");
+        case 4:printf("food item-French Fries :)\nprice-Rs99");
         break; 
-        case 5:printf("food item-Sandwich\nprice-Rs149");
+        case 5:printf("food item-Sandwich :)\nprice-Rs149");
         break;
-        default:printf("the food item u chose is not in the menu \nsorry!");
+        default:printf("the food item u chose is not in the menu \nsorry :(");
     }
     }
 
