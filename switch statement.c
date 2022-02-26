02c7e56a7c40b9adf9ed300432b6eaaa98a3dea3
PRAGNA1816 <96616690+PRAGNA1816@users.noreply.github.com>
2022-02-26 19:18:42 +0530

    Create switch statement.c

---
+++ switch statement.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main()
+{
+    int food_item;
+    scanf("%d",&food_item);
+    switch(food_item)
+    {
+        case 1:printf("food item-Pizza\nprice-Rs239");
+        break;
+        case 2:printf("food item-Burger\nprice-Rs129");
+        break;
+        case 3:printf("food item-Pasta\nprice-Rs179");
+        break;
+        case 4:printf("food item-French Fries\nprice-Rs99");
+        break; 
+        case 5:printf("food item-Sandwich\nprice-Rs149");
+        break;
+        default:printf("the food item u chose is not in the menu \nsorry!");
+    }
+    }
+
